1326ca2c12ed7b30b7ec3054ed0e796f6962a292
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-06 21:40:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   return object;
 }
 
+var obj = { foo: 'bar' };
+var newObj = Object.assign({}, obj)
+
+newObj // { foo: 'bar
+ 
+delete newObj.foo // true
+ 
+newObj // {}
