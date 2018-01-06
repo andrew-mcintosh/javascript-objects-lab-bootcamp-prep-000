3ab891b6e84fbe95a20c155ea4510c212599e321
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-06 21:31:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object); 
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
